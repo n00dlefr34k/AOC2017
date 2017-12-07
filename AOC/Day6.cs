93fe6a28243c041df7b6a39fb40b0faac5bd8a52
pkrys14k <pkrysiak@networkats.com>
2017-12-06 22:20:57 -0500

    cleaning upo the code and adding comments so i can follow later

--- AOC/Day6.cs
+++ AOC/Day6.cs
@@ -13,30 +13,31 @@ namespace AOC
     {
         public static void day6Init()
         {
-           
-            SqlConnection conn =  new SqlConnection(@"server=AMBROSIA\CHARLOTTE;database=AOC2017;user=AOC2017;pwd=AOC2017;MultipleActiveResultSets=True;");
-
-            string cleanTable = "truncate table[day6]";
-
-            DatabaseFactory.UpdateDeleteProcedure(cleanTable, conn);
+            /*sql degbugging 
+             SqlConnection conn =  new SqlConnection(@"server=AMBROSIA\CHARLOTTE;database=AOC2017;user=AOC2017;pwd=AOC2017;MultipleActiveResultSets=True;");
+             string insertStatement = "INSERT INTO[day6]([arrayNunber])VALUES('{0}')";
+             string cleanTable = "truncate table[day6]";
+             DatabaseFactory.UpdateDeleteProcedure(cleanTable, conn);*/
 
             int[] block = new int[] { 0, 2, 7, 0 };
-
-            string insertStatement = "INSERT INTO[day6]([arrayNunber])VALUES('{0}')";
-
             int[] blocks = new int[] { 2, 8, 8, 5, 4, 2, 3, 1, 5, 5, 1, 2, 15, 13, 5, 14 };
 
             List<int[]> existing = new List<int[]>();
             bool match = false;
             int count = 0;
+            // for sql debugging
             //while (count < 30000)
             while (!match)
             {
+                // must do the clone thing or all the values in the list get chnaged when you change the array values
                 existing.Add((int[])blocks.Clone());
 
                 int max = blocks.Max();
                 int index = Array.IndexOf(blocks, max);
+                // set  the max index to 0
                 blocks[index] = 0;
+                //dispurse the max value among the rest of the 
+                //indexes startign at the next index from the max value
                 while (max > 0)
                 {
                     index++;
@@ -44,20 +45,22 @@ namespace AOC
                     blocks[temp3] += 1;
                     max--;
                 }
-                // fill the db with all the values, much faster 
-                DatabaseFactory.UpdateDeleteProcedure(String.Format(insertStatement, JsonConvert.SerializeObject(blocks)), conn);
+                // fill the db with all the values, much faster then code just to see if i was on the right track
+                //DatabaseFactory.UpdateDeleteProcedure(String.Format(insertStatement, JsonConvert.SerializeObject(blocks)), conn);
+
+                //drop the list into an arraqy for preformance and list can't do what i need them to do contain didn't do a thing on this one.
                 int[][] temp = existing.ToArray();
                 for (int x = 0; x < temp.Length; x++)
                 {
                     for (int y = 0; y < temp.Length; y++)
                     {
-                        // this didn't work with large arrays, i had to do it in the database
+                        // check if the sequesnce is hte same
                         if (x != y && temp[x].SequenceEqual(temp[y]))
                         {
                             match = true;
                             Console.WriteLine("distance between " + Math.Abs(x - y));
                         }
-                           
+
                     }
                 }
 
@@ -67,37 +70,6 @@ namespace AOC
             }
             Console.WriteLine("steps " + --count);
         }
-
-        private static void printblocks(int[] blocks)
-        {
-            for (int x = 0; x < blocks.Length; x++)
-            {
-                Console.Write(blocks[x] + " ");
-            }
-            Console.WriteLine(" ");
-        }
-
-        /* sql aggregate the links */
-        //      select* ,
-        //(Select top 3 CAST(f.id as nvarchar)+ ',' AS[text()],
-        //ROW_NUMBER() OVER(ORDER BY columnid) AS ROW_NUM
-        //              From dbo.[day6] as f
-        //              Where f.[arrayNunber] = table3.[arrayNunber] and f.ROW_NUM > 1
-        //              ORDER BY f.id
-        //              For XML PATH ('')
-        //          ) as ids
-
-        //from(SELECT[arrayNunber],
-        //              COUNT(*) as countt
-        //          FROM
-
-        //              [day6]
-        //          GROUP BY
-
-        //              [arrayNunber]
-        //          HAVING
-        //              COUNT(*) > 1 
-        //		) as table3 order by ids
 
     }
 }
